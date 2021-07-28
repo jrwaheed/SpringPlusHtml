73e10ca8ece15f0e839ca769091ccb03b092eabd
jrwaheed <j.r.waheed@gmail.com>
2021-07-28 18:12:16 +0200

    @GeneratedValue(strategy = GenerationType.IDENTITY

--- src/main/java/com/example/SpringPlusHtml/domain/Customer.java
+++ src/main/java/com/example/SpringPlusHtml/domain/Customer.java
@@ -9,8 +9,7 @@ import javax.persistence.Id;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String lastName;
     private String firstName;
